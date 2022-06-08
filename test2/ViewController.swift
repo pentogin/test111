84bee7b302f3da46c5e1dd23b3cf156501a5ecfc
pentogin <spaicy.suger@hotmail.co.jp>
2022-06-08 23:02:39 +0900

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 小竹啓太 on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
